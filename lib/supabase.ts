a02a34c1dfb266f42c8ac56ed0c9fa05a528b5d8
sgandulfo-code <sgandulfo@remax.com.ar>
2026-02-11 13:42:22 -0300

    Create supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+// Estas son las variables que configuraste en Vercel
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
